aba408ab1c1db42314241108d1d9154a8b170b8c
Diego Labrin <di3golabrin@gmail.com>
2026-04-20 22:41:00 -0400

    chore: cambio organización a pallet agregando 5 repositorios y ajuste en notebook

--- ciberseguridad_2026/data/repos/rest-framework-tutorial/tutorial/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-
-urlpatterns = [
-    path('', include('snippets.urls')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'))
-]
